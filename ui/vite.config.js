0eff9223c01978f64696df5aecd9a26a8286c6f0
diki816 <dikisun@gmail.com>
2025-10-20 03:30:00 +0900

    backend api url changed to render.com

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://cozy-backend-ksq3.onrender.com',
         changeOrigin: true,
       },
     },
